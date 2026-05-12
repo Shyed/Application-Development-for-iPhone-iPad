1bcd89b3a321c020a1ad9502cea3187a63638fba
SDemonteverde <38735789+Shyed@users.noreply.github.com>
2026-05-12 13:01:08 -0400

    Update ViewController.swift

--- Lab 5/ViewController.swift
+++ Lab 5/ViewController.swift
@@ -1,51 +1,86 @@
-//
-//  ViewController.swift
-//  Lab 5
-//
-//  Created by Sheila Demonteverde on 12/8/20.
-//  Copyright © 2020 Sheila Demonteverde. All rights reserved.
-//
+/*
+=====================================================
+ PROGRAM: ViewController.swift
+ PROJECT: Lab 5
+
+ DESCRIPTION:
+ This ViewController manages camera functionality
+ within the iOS application.
+
+ FEATURES:
+ - Opens device camera
+ - Captures photos using UIImagePickerController
+ - Displays selected image in UIImageView
+ - Handles image picker cancellation
+
+ FRAMEWORKS:
+ - UIKit
+
+ NOTES:
+ - Requires camera permissions in Info.plist
+ - Uses UIImagePickerControllerDelegate
+ - Designed for beginner iOS development practice
+
+Author: Sheila Demonteverde | 12/8/2020
+Updated Dev Notes: 05/12/2026 - SD
+=====================================================
+*/
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController 
+{
+    /*-- UI COMPONENTS --*/
+    @IBOutlet var imageView: UIImageView! // Image view for displaying captured image
 
-    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var button: UIButton! // Button used to launch camera
 
-    @IBOutlet var button: UIButton!
-    
-    override func viewDidLoad() {
+    /*-- VIEW DID LOAD --*/
+    override func viewDidLoad() 
+    {
         super.viewDidLoad()
-        
+
+        // Set image view background color
         imageView.backgroundColor = .secondarySystemBackground
-        
+
+        // Customize button appearance
         button.backgroundColor = .systemBlue
         button.setTitle("Take Picture", for: .normal)
         button.setTitleColor(.white, for: .normal)
     }
-    
-    @IBAction func didTapButton (){
-        let picker = UIImagePickerController()
-        picker.sourceType = .camera
-        picker.delegate = self
-        present(picker, animated: true)
+
+    /*-- CAMERA BUTTON ACTION --*/
+    @IBAction func didTapButton ()
+    {
+        let picker = UIImagePickerController() // Create image picker controller
+        picker.sourceType = .camera // Set source to device camera
+        picker.delegate = self // Assign delegate
+        present(picker, animated: true) // Present camera interface
     }
 }
 
-extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
+/*-- IMAGE PICKER EXTENSION --*/
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate 
+{
+     /*-- CANCEL IMAGE PICKER --*/
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) 
+    {
+        picker.dismiss(animated: true, completion: nil) // Close camera interface
     }
-    
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
+
+    /*-- HANDLE CAPTURED IMAGE --*/
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) 
+    {
+        // Close image picker after selection
         picker.dismiss(animated: true, completion: nil)
-        
+
+        // Retrieve selected image safely
         guard let image = info[UIImagePickerController.InfoKey.originalImage] as?
             UIImage else{
                 return
         }
+
+        // Display image in image view
         imageView.image = image
     }
 }
